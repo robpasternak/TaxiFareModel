0c131ab4daf020fd05a61f22ad641633c466827d
Robert Pasternak <pasternakrs@gmail.com>
2022-02-22 16:19:10 +0100

    Finished MLFlow Quickstart challenge

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,5 +1,7 @@
 # imports
 import numpy as np
+import mlflow
+from mlflow.tracking import MlflowClient
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -8,9 +10,10 @@ from sklearn.compose import ColumnTransformer
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.data import get_data, clean_data
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
 
 class Trainer():
-    def __init__(self, X, y):
+    def __init__(self, X, y, experiment_name = ''):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -18,6 +21,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = experiment_name
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -51,8 +55,32 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         return compute_rmse(y_pred, y_test)
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
 if __name__ == "__main__":
+    EXPERIMENT_NAME = "[DE] [Berlin] [robpasternak] TaxiFareModel v1"
+
     # get data
     df = get_data()
 
@@ -67,9 +95,10 @@ if __name__ == "__main__":
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
 
     # train
-    trainer = Trainer(X_train, y_train)
+    trainer = Trainer(X_train, y_train, EXPERIMENT_NAME)
     trainer.run()
     rmse = trainer.evaluate(X_val, y_val)
 
-    # evaluate
-    print(rmse)
+    # create mlflow client
+    trainer.mlflow_log_metric("rmse", 4.5)
+    trainer.mlflow_log_param("model", 'linear')
